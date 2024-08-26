dd3f3b778467aa9be2dc5ce192bb9b2b96828c5c
kofiadaba <kofiadaba@gmail.com>
2024-08-26 11:18:58 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div>
-      <h1>Hello WORLD!</h1>
-
-    </div>
+    <div>App</div>
   )
 }
-
-export default App
