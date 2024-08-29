869b0a601ca04576feb1f3042ad676e25c4dc37e
kofiad <kofiadaba@gmail.com>
2024-08-29 15:24:45 +0000

    Update page title to "Tic-Tac-Toe"

--- web_tic_tac_toe/src/App.jsx
+++ web_tic_tac_toe/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='bg-green-500 border-4 rounded-lg '>
-      <h1>
+    <div className='w-full h-full items-center justify-center bg-green-500 border-4 rounded-lg '>
+      <h1 className='text-center font-bold text-4xl'>
         Tic-Tac-Toe
       </h1>
     </div>
