d4a34f651ec87557255a5300e34f890bbdeafd02
kofiad <kofiadaba@gmail.com>
2024-08-29 16:29:22 +0000

    chore: Update App component styling for tic-tac-toe game

--- web_tic_tac_toe/src/App.jsx
+++ web_tic_tac_toe/src/App.jsx
@@ -4,7 +4,7 @@ import Game from './components/Game'
 export default function App() {
   return (
     <div className='w-full p-4 items-center justify-center bg-slate-300 border-4 rounded-lg '>
-      <div className='w-full items-center justify-center bg-green-500 border-4 rounded-lg '>
+      <div className='w-full p-4 items-center justify-center bg-green-500 border-4 rounded-lg '>
         <h1 className='text-center font-bold text-4xl'>
           Tic-Tac-Toe
         </h1>
